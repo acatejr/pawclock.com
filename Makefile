94a2e39a36361fce858fce46237a5647609cf7bf
acatejr@gmail.com <acatejr@gmail.com>
2025-04-16 16:49:40 -0700

    Testing ready for owners and pets

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@ format:
 	uv run ruff format .
 
 test:
-	uv run pytest -v
+	uv run pytest -vs
 
 serve:
 	uv run fastapi dev main.py
 
 test-models:
-	uv run pytest -v tests/test_models.py
+	uv run pytest -vs tests/test_models.py
 
 all: format test
--- models.py
+++ models.py
@@ -1,3 +1,40 @@
+# from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
+from sqlalchemy import ForeignKey, String, Integer
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Owner(Base):
+    __tablename__ = 'owners'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=True)
+    phone = Column(String, nullable=False)
+    pets = relationship("Pet", secondary="owners_pets", back_populates="owners")
+
+
+class Pet(Base):
+    __tablename__ = 'pets'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    owners = relationship("Owner", secondary="owners_pets", back_populates="pets")
+
+
+class OwnerPet(Base):
+    __tablename__ = 'owners_pets'
+    owner_id = Column(Integer, ForeignKey('owners.id'), primary_key=True)
+    pet_id = Column(Integer, ForeignKey('pets.id'), primary_key=True)
+
+
+# Create a SQLite database in memory for demonstration purposes
+# engine = create_engine('sqlite:///:memory:')
+
 # from typing import Optional
 # from sqlmodel import SQLModel, Field
 
