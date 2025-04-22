9d443dea839952ea21e16b317f9f09f2b0b38515
acatejr@gmail.com <acatejr@gmail.com>
2025-04-22 10:44:36 -0700

    Finished adding landing page that lists open day care session

--- pawclock/app_pawclock/urls.py
+++ pawclock/app_pawclock/urls.py
@@ -17,10 +17,10 @@ Including another URLconf
 
 from django.urls import path
 
-from . import views
+from .views import IndexView
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", IndexView.as_view(), name="index"),
     # path("about/", views.about, name="about"),
     # path("contact/", views.contact, name="contact"),
     # path("privacy/", views.privacy, name="privacy"),
--- pawclock/app_pawclock/views.py
+++ pawclock/app_pawclock/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import DayCareSession
 
 # Create your views here.
-def index(request):
+# def index(request):
+#     """
+#     Render the index page.
+#     """
+#     return render(request, "index1.html")
+
+class IndexView(ListView):
     """
     Render the index page.
     """
-    return render(request, "index.html")
+    model = DayCareSession
+    template_name = "index.html"
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(IndexView, self).get_queryset(*args, **kwargs)
+        qs = DayCareSession.objects.filter(check_out__isnull=True)
+        # qs = qs.order_by("-id")
+        return qs
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(IndexView, self).get_context_data(**kwargs)
+    #     #filter_set = DayCareSession.objects.all()
+
+        # filter_set = Gauges.objects.all()
+        # if self.request.GET.get('gauge_id'):
+        #     gauge_id = self.request.GET.get('gauge_id')
+        #     filter_set = filter_set.filter(gauge_id=gauge_id)
+
+
+# class BookListView(ListView):
+#     model = Book
+
+#     def head(self, *args, **kwargs):
+#         last_book = self.get_queryset().latest("publication_date")
+#         response = HttpResponse(
+#             # RFC 1123 date format.
+#             headers={
+#                 "Last-Modified": last_book.publication_date.strftime(
+#                     "%a, %d %b %Y %H:%M:%S GMT"
+#                 )
+#             },
+#         )
+#         return response
 def about(request):
     """
     Render the about page.
