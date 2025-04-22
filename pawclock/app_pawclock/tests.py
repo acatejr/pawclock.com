b63b2dfb385111434ca31e9ea4c9b613f816a116
acatejr@gmail.com <acatejr@gmail.com>
2025-04-22 15:48:40 -0700

    First work on this branch - cleanup

--- pawclock/app_pawclock/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-import pytest
-
-
-def test_example():
-    assert True  # Replace with your actual test logic
-
-    pytest.fail("This test is not implemented yet")
