09eb8ed0151db8662ca5015aedd98328818c95dd
acatejr@gmail.com <acatejr@gmail.com>
2025-04-22 16:45:39 -0700

    Started move to Elixir/Phoenix

---
+++ app/lib/app_web/controllers/page_controller.ex
@@ -0,0 +1,15 @@
+defmodule AppWeb.PageController do
+  use AppWeb, :controller
+
+  def index(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :index, layout: false)
+  end
+
+  def about(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :about, layout: false)
+  end
+end
